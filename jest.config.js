6bfd4b97c57c40ceb5ee8d51db333dd8b12fb721
Derick Porter <Derick.Porter@outlook.com>
2026-01-07 22:41:19 -0800

    fix: Exclude Sentinel-main directory from Jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ import { jestConfig } from "@salesforce/sfdx-lwc-jest/config.js";
 
 export default {
   ...jestConfig,
-  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver", "<rootDir>/Sentinel-main/"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/Sentinel-main/"],
 };
